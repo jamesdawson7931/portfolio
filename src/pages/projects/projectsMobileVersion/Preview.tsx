b3de5afeba15488b51c3e96554801ca2bb6d8eea
dawsonjay <dawsonjames027@gmail.com>
2025-08-13 01:00:59 +0100

    formatted mobile preview a bit

--- src/pages/projects/projectsMobileVersion/Preview.tsx
+++ src/pages/projects/projectsMobileVersion/Preview.tsx
@@ -1,9 +1,11 @@
 import "./styles/preview.scss";
 import type { ProjectPreview } from "../projectsDesktopVersion/projectData/projectPreviews.ts";
 import { Link } from "react-router-dom";
+import cn from "classnames";
 
 interface PreviewProps extends ProjectPreview {
   isSelected: boolean;
+  onClick: (id: string) => void;
 }
 
 export function Preview({
@@ -14,15 +16,17 @@ export function Preview({
   dateCreated,
   isSelected,
   id,
+    onClick,
 }: PreviewProps) {
+  const selectedClass = isSelected ? "selected" : "";
   return (
-    <div className={"preview"}>
-      <div className={"preview-card"}>
+    <div className={cn("preview", selectedClass)} onClick={() => onClick(id)}>
+      <div className={cn("preview-card", selectedClass)}>
         <h3 className={"title"}>{title}</h3>
         <p className={"date-created"}>{dateCreated}</p>
         <article className={"short-intro"}>{shortIntro}</article>
       </div>
-      <div className={"full-preview"}>
+      <div className={cn("full-preview", selectedClass)} >
         <img src={previewImage} alt={title} className={"preview-image"} />
         <article className={"long-intro"}>{longIntro}</article>
         <Link className="read-on-button" to={id}>
--- src/pages/projects/projectsMobileVersion/ProjectsMobileVersion.tsx
+++ src/pages/projects/projectsMobileVersion/ProjectsMobileVersion.tsx
@@ -3,13 +3,18 @@ import Page from "../../Page.tsx";
 import Title from "./Title.tsx";
 import { previews } from "../projectsDesktopVersion/projectData/projectPreviews.ts";
 import { Preview } from "./Preview.tsx";
+import {useState} from "react";
+
+const previewIds = previews.map((preview) => preview.id);
 
 export function ProjectsMobileVersion() {
+    const [selectedProject, setSelectedProject] = useState(previewIds[0]);
+
   return (
     <Page className={"projects-mobile-version-page"}>
       <Title />
       {previews.map((preview) => (
-        <Preview isSelected={false} {...preview} />
+        <Preview isSelected={selectedProject === preview.id} {...preview} onClick={setSelectedProject}/>
       ))}
     </Page>
   );
